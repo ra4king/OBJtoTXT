6d650ed6e068979bfbef7968ad5f0a8baa477194
Roi Atalla <roi@roiatalla.com>
2015-10-06 03:56:23 -0400

    Fixed a few bugs.

--- obj_to_text.c
+++ obj_to_text.c
@@ -4,22 +4,23 @@ void parse_obj(FILE* in, FILE* out) {
 	char buffer[512];
 
 	while(!feof(in)) {
-		int b1 = fgetc(in);
-		int b2 = fgetc(in);
-		int startAddress = (b1 << 8) | b2;
+		int header_num_read = fread(buffer, 1, 4, in);
+		if(header_num_read != 4)
+			break;
+		
+		int startAddress = (buffer[0] << 8) | buffer[1];
 
-		int c1 = fgetc(in);
-		int c2 = fgetc(in);
-		int count = ((c1 << 8) | c2) * 2;
+		int count = ((buffer[2] << 8) | buffer[3]) * 2;
 
 		fprintf(out, ".orig %04X\n", startAddress);
 		fprintf(out, "length: %d\n", count);
 
 		while(count && !feof(in)) {
-			int num_read = fread(buffer, 1, sizeof(buffer), in);
+			int to_read = sizeof(buffer) < count ? sizeof(buffer) : count;
+			int num_read = fread(buffer, 1, to_read, in);
 			count -= num_read;
 
-			for(int i = 0; i < num_read - 1; i += 2) {
+			for(int i = 0; i < num_read; i += 2) {
 				fprintf(out, "%04X\t", ((buffer[i] << 8) | buffer[i+1]) & 0xFFFF);
 				if((i+2) % 10 == 0)
 					fprintf(out, "\n");
